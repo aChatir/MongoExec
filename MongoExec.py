3c4ff4a4da43b12d13aa9f3f6b466a6f3f997b63
Abdelaziz Chatir <achatir@acewebdesign.com.au>
2014-09-28 14:28:05 +0100

    Initial commit

---
+++ MongoExec.py
@@ -0,0 +1,179 @@
+import sublime, sublime_plugin, tempfile, os, subprocess
+
+connection = None
+connectionName = ''
+collection = ''
+history = ['']
+
+class Connection:
+    def __init__(self, options):
+        self.settings = sublime.load_settings(options.type + ".mongoexec").get('mongo_exec')
+        self.command  = sublime.load_settings("MongoExec.sublime-settings").get('mongo_exec.commands')[options.type]
+        self.options  = options
+
+    def _buildCommand(self, query):
+        global collection
+        if 'find' in query:
+            query = query + ".toArray()"
+        return self.command + ' ' + collection + " --eval \"printjson(%s)\"" % query
+
+    def _getCommand(self, queryN):
+        command  = self._buildCommand(queryN)
+        self.tmp = tempfile.NamedTemporaryFile(mode = 'w', delete = False, suffix='.mongo')
+        self.tmp.write(queryN + "\n")
+        self.tmp.close()
+
+        cmd = '%s < "%s"' % (command, self.tmp.name)
+
+        return Command(cmd)
+
+    def execute(self, query):
+        command = self._getCommand(query)
+        command.show()
+        os.unlink(self.tmp.name)
+
+class Command:
+    def __init__(self, text):
+        self.text = text
+
+    def _display(self, panelName, text):
+        if not sublime.load_settings("MongoExec.sublime-settings").get('show_result_on_window'):
+            panel = sublime.active_window().create_output_panel(panelName)
+            sublime.active_window().run_command("show_panel", {"panel": "output." + panelName})
+        else:
+            panel = sublime.active_window().new_file()
+
+        panel.set_read_only(False)
+        panel.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
+        panel.run_command('append', {'characters': text})
+        panel.set_read_only(True)
+
+    def _result(self, text):
+        self._display('MongoExec', text)
+
+    def _errors(self, text):
+        self._display('MongoExec.errors', text)
+
+    def run(self):
+        sublime.status_message(' MongoExec: running Mongo command')
+        results, errors = subprocess.Popen(self.text, stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True).communicate()
+
+        if not results and errors:
+            self._errors(errors.decode('utf-8', 'replace').replace('\r', ''))
+
+        return results.decode('utf-8', 'replace').replace('\r', '')
+
+    def show(self):
+        results = self.run()
+
+        if results:
+            self._result(results)
+
+class Selection:
+    def __init__(self, view):
+        self.view = view
+    def getQueries(self):
+        text = []
+        if self.view.sel():
+            for region in self.view.sel():
+                if region.empty():
+                    text.append(self.view.substr(self.view.line(region)))
+                else:
+                    text.append(self.view.substr(region))
+
+        return text
+
+class Options:
+    def __init__(self, name):
+        '''mongodb://[username:passwor)d@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]'''
+        self.name     = name
+        connections   = sublime.load_settings("MongoExec.sublime-settings").get('connections')
+        self.type     = connections[self.name]['type']
+        self.host     = connections[self.name]['host']
+        self.port     = connections[self.name]['port']
+        self.username = connections[self.name]['username']
+        self.password = connections[self.name]['password']
+        self.databases = connections[self.name]['databases']
+        if 'service' in connections[self.name]:
+            self.service  = connections[self.name]['service']
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def list():
+        names = []
+        connections = sublime.load_settings("MongoExec.sublime-settings").get('connections')
+        for connection in connections:
+            names.append(connection)
+        names.sort()
+        return names
+
+    def listDatabases():
+        names = []
+        connections = sublime.load_settings("MongoExec.sublime-settings").get('connections')
+        for dbName in connections[connectionName]['databases']:
+            names.append(dbName)
+        names.sort()
+        return names
+
+
+def mongoChangeConnection(index):
+    global connection, connectionName
+    names = Options.list()
+    options = Options(names[index])
+    connectionName = names[index]
+    connection = Connection(options)
+    sublime.status_message(' MongoExec: switched to %s connection' % names[index])
+    sublime.active_window().run_command("mongo_list_collection", {})
+
+
+def mongoChangeDB(index):
+    global collection
+    names = Options.listDatabases()
+    collection = names[index]
+    sublime.status_message(' MongoExec: switched to %s collection' % names[index])
+
+def executeHistoryQuery(index):
+    global history
+    if index > -1:
+        executeQuery(history[index])
+
+def executeQuery(query):
+    global connection
+    global history
+    history.append(query)
+    if connection != None:
+        connection.execute(query)
+
+class mongoHistory(sublime_plugin.WindowCommand):
+    global history
+    def run(self):
+        sublime.active_window().show_quick_panel(history, executeHistoryQuery)
+
+class mongoQuery(sublime_plugin.WindowCommand):
+    def run(self):
+        global connection
+        global history
+        if connection != None:
+            sublime.active_window().show_input_panel('Enter query', history[-1], executeQuery, None, None)
+        else:
+            sublime.error_message('No active connection')
+
+class mongoExecute(sublime_plugin.WindowCommand):
+    def run(self):
+        global connection
+        if connection != None:
+            selection = Selection(self.window.active_view())
+            connection.execute(selection.getQueries())
+        else:
+            sublime.error_message('No active connection')
+
+class mongoListConnection(sublime_plugin.WindowCommand):
+    def run(self):
+        sublime.active_window().show_quick_panel(Options.list(), mongoChangeConnection)
+
+class mongoListCollection(sublime_plugin.WindowCommand):
+    def run(self):
+        sublime.set_timeout(lambda: sublime.active_window().show_quick_panel(Options.listDatabases(), mongoChangeDB), 10)
+
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 MongoExec
-=========
 
 A sublime text 3 plugin to execute mongodb commands
+
+
+A Plugin for running Mongo commands in Sublime Text.
+
+
+## Authors
+
+* Abdelaziz CHATIR
+
+## Installation and Configuration
+
+[Full documentation on official site](https://github.com/kalo7791/MongoExec)
